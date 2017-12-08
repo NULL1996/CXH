655d8ec45910c1372c48694226a8db61faa14eee
NULL1996 <1227846207@qq.com>
2017-12-08 11:16:25 +0800

    today wzw

--- Web/index.aspx.designer.cs
+++ Web/index.aspx.designer.cs
@@ -7,11 +7,13 @@
 // </自动生成>
 //------------------------------------------------------------------------------
 
-namespace Web {
-    
-    
-    public partial class index {
-        
+namespace Web
+{
+
+
+    public partial class index
+    {
+
         /// <summary>
         /// TextBox1 控件。
         /// </summary>
@@ -20,7 +22,7 @@ namespace Web {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
+
         /// <summary>
         /// Button1 控件。
         /// </summary>
@@ -29,7 +31,7 @@ namespace Web {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
-        
+
         /// <summary>
         /// ListView1 控件。
         /// </summary>
@@ -38,7 +40,7 @@ namespace Web {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView ListView1;
-        
+
         /// <summary>
         /// Button2 控件。
         /// </summary>
